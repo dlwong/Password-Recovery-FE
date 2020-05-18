2d249e0fdf1822a1d6c87aba36a368d13cbde368
dlwong <starletfordorris@gmail.com>
2020-05-18 00:54:51 -0700

    Implement FE error handling for reset password screen

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -5,6 +5,7 @@ import {
   UPDATE_FIELD_AUTH,
   RESET_PASSWORD
 } from '../constants/actionTypes';
+import axios from 'axios';
 
 const mapStateToProps = state => ({ ...state.auth });
 
@@ -13,8 +14,8 @@ const mapDispatchToProps = dispatch => ({
     dispatch({ type: UPDATE_FIELD_AUTH, key: 'newPassword', value }),
   onChangeConfirmPassword: value =>
     dispatch({ type: UPDATE_FIELD_AUTH, key: 'confirmPassword', value }),
-  onSubmit: (password) =>
-    dispatch({ type: RESET_PASSWORD, payload: agent.Auth.verifyPassword(password) })
+  onSubmit: (username, password) =>
+    dispatch({ type: RESET_PASSWORD, payload: agent.Auth.verifyPassword(username, password) })
 });
 
 class ResetPassword extends React.Component {
@@ -22,7 +23,12 @@ class ResetPassword extends React.Component {
     super();
 
     this.state = {
-              matchingPasswords: true
+              matchingPasswords: true,
+              username:'',
+              password:'',
+              updated: false,
+              error: false,
+              canUpdate: false
     }
     this.changeNewPassword = ev => this.props.onChangeNewPassword(ev.target.value);
     this.changeConfirmPassword = ev => this.props.onChangeConfirmPassword(ev.target.value);
@@ -30,21 +36,51 @@ class ResetPassword extends React.Component {
       ev.preventDefault();
       if (newPassword === confirmPassword) {
         this.setState({matchingPasswords: true})
-        this.props.onSubmit(confirmPassword);
+        this.props.onSubmit(this.state.username, confirmPassword);
       }else {
         this.setState({matchingPasswords: false})
       }
     };
   }
 
+  componentDidMount() {
+    axios
+      .get('http://localhost:3000/api/resetpassword', {
+            params: {
+          token: this.props.match.params.token
+          }
+      })
+      .then(response => {
+        if (response.data.message === 'password link is fine'){
+          this.setState({
+            username: response.data.username,
+            canUpdate: true,
+            error: false,
+          })
+        } else {
+          this.setState({
+            canUpdate: false,
+            error: true
+          })
+        }
+      })
+      .catch(error => {
+        console.log(error.data)
+      })
+  }
+
   render() {
     const newPassword = this.props.newPassword;
     const confirmPassword = this.props.confirmPassword;
+    const { error, canUpdate } = this.state;
     return (
       <div className="auth-page">
         <div className="container page">
           <div className="row">
-
+          {error &&
+        <div>Problem resetting, please submit a new reset form</div>
+        }
+        {canUpdate && 
             <div className="col-md-6 offset-md-3 col-xs-12">
               <h1 className="text-xs-center">Reset Password</h1>
               <p className="text-xs-center">
@@ -85,7 +121,7 @@ class ResetPassword extends React.Component {
                 </fieldset>
               </form>
             </div>
-
+          }
           </div>
         </div>
       </div>
